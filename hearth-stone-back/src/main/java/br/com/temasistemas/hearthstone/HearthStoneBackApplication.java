f30dac9e7e4e7a1c4ec33461555fb519184391b5
Jonas Góes <jonasdossantos1991@gmail.com>
2021-08-27 15:40:40 -0300

    criado o projeto

---
+++ hearth-stone-back/src/main/java/br/com/temasistemas/hearthstone/HearthStoneBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.temasistemas.hearthstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearthStoneBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearthStoneBackApplication.class, args);
+	}
+
+}
---
+++ hearth-stone-back/src/test/java/br/com/temasistemas/hearthstone/HearthStoneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.temasistemas.hearthstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearthStoneBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
