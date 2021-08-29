f831d8fbf645a9ab7f5548068d9dcf1408b30279
Jonas Góes <jonasdossantos1991@gmail.com>
2021-08-29 16:41:07 -0300

    alteração da imagens dos personagems

--- hearth-stone-front/src/app/models/Carta.model.ts
+++ hearth-stone-front/src/app/models/Carta.model.ts
@@ -13,22 +13,22 @@ export const TIPO = ['MAGIA', 'CRIATURA'];
 export const IMAGENS = [
     {
         classe:'MAGO',
-        link:'https://pm1.narvii.com/6458/c1c77a74d37cb73855ef9b1107162b75404b9f8a_hq.jpg'
+        link:'https://static.wikia.nocookie.net/hearthstone_gamepedia/images/3/3c/Jaina_Proudmoore%28320%29.png'
     },
     {
         classe:'PALADINO',
-        link:'https://pm1.narvii.com/6547/0cf7c1dd2714ef107ab2953f308b28be6e90d5d5_hq.jpg'
+        link:'https://static.wikia.nocookie.net/hearthstone_gamepedia/images/4/4d/Uther_Lightbringer%28257%29.png'
     },
     {
         classe:'CACADOR',
-        link:'https://i.pinimg.com/originals/94/3b/f1/943bf1b79dcab3d2b529e904c15a76c9.jpg'
+        link:'https://static.wikia.nocookie.net/hearthstone_gamepedia/images/a/a0/Rexxar%28484%29.png'
     },
     {
         classe:'DRUIDA',
-        link:'http://4.bp.blogspot.com/_wnuwcUW6qdE/TUlHvwKNr9I/AAAAAAAAAqI/RSWd3mJmd64/s1600/Druid_by_Okha.jpg'
+        link:'https://static.wikia.nocookie.net/hearthstone_gamepedia/images/f/fa/Malfurion_Stormrage%28621%29.png'
     },
     {
         classe:'QUALQUER',
-        link:'https://i.pinimg.com/originals/1e/ea/6e/1eea6ee48a55cd76efbc19bf6bb1644f.jpg'
+        link:'https://static.wikia.nocookie.net/hearthstone_gamepedia/images/c/c5/Garrosh_Hellscream%28635%29.png'
     }
 ];
