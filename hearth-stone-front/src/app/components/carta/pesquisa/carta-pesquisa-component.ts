d7e825280d5b88feb9ba708d67f17da4d8b27e87
Jonas Góes <jonasdossantos1991@gmail.com>
2021-08-29 10:22:54 -0300

    adicionado botão de remoção

--- hearth-stone-front/src/app/components/carta/pesquisa/carta-pesquisa-component.ts
+++ hearth-stone-front/src/app/components/carta/pesquisa/carta-pesquisa-component.ts
@@ -9,12 +9,14 @@ import { Component } from "@angular/core";
 export class CartaPesquisaComponent {
     public info: any = null;
     public cartas: Carta[]
+    private carta:Carta;
     //public atributos = ['ataque', 'defesa', 'tipo', 'classe'];
     public imagens = IMAGENS;
     public pesquisado = false;
     constructor(private cartaService: CartaService,private router:Router) { }
 
     pesquisar(carta: Carta) {
+        this.carta = carta;
         this.cartaService.procurar(this.getParams(carta)).subscribe(resp => {
             this.cartas = resp.length ? resp : null;
             this.pesquisado = true;
@@ -31,6 +33,18 @@ export class CartaPesquisaComponent {
     
     detalhe(id:number){
         this.router.navigate(['detalhe',id]);
+    }
+
+    deletar(id:number){
+        this.cartaService.deletar(id).subscribe(()=>{
+            this.info = {};
+            this.info.message = "Carta removida com sucesso!";
+            this.info.alerta = 'alert-success';
+        })
+        setTimeout(()=>{
+            this.info = null;
+            this.pesquisar(this.getParams(this.carta));
+        },3000)
     }
 
     private getParams(carta: Carta) {
